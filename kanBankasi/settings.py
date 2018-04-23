08e2f083f8f0691cf13bf082ed69fc936a65b701
sumeyyeozturkk <sumeyyeozturk@Sumeyye-MacBook-Pro.local>
2018-04-23 16:26:01 +0300

    Veri tabanı bağlantı ayarları gerçekleştirildi.

--- kanBankasi/settings.py
+++ kanBankasi/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'kanBankasi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE':'django.db.backends.postgresql',
         'NAME': 'KanBankasi',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
