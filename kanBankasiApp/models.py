01da1be6c3dd3d3be9bc065b4e2c69bfc04e29a2
sumeyyeozturkk <sozturk15@posta.pau.edu.tr>
2018-04-23 17:07:03 +0300

    Ilce modeli oluşturuldu.

--- kanBankasiApp/models.py
+++ kanBankasiApp/models.py
@@ -8,6 +8,13 @@ class Il(models.Model):
 	def __str__(self):
 		return self.il_adi
 
+class Ilce(models.Model):
+	ilce_adi = models.CharField(max_length = 30)
+	il_id = models.ForeignKey(Il, on_delete= models.PROTECT)
+
+	def __str__(self):
+		return self.ilce_adi
+
 
 
 
