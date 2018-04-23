d44890678aff4f133ee2697d0637b78692ff3ec4
sumeyyeozturkk <sumeyyeozturk@Sumeyye-MacBook-Pro.local>
2018-04-23 16:58:50 +0300

    Il modeli oluşturuldu.

--- kanBankasiApp/models.py
+++ kanBankasiApp/models.py
@@ -1,2 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Il(models.Model):
+	il_adi = models.CharField(max_length = 30)
+
+	def __str__(self):
+		return self.il_adi
+
+
+
+
+
 
