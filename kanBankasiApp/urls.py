c405d0987709c33646125c6a24a9ce03af7fa3f6
sumeyyeozturkk <sozturk15@posta.pau.edu.tr>
2018-05-01 14:46:51 +0300

    urls.py değiştirme

--- kanBankasiApp/urls.py
+++ kanBankasiApp/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
+	url(r'^login/$', auth_views.login, {'template_name': 'kanBankasiApp/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 ]
