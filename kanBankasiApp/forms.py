10d5e91256688d028009fabd82e8561d944e25a4
sumeyyeozturkk <sozturk15@posta.pau.edu.tr>
2018-05-02 01:41:41 +0300

    Formda değişiklik yapıldı.

--- kanBankasiApp/forms.py
+++ kanBankasiApp/forms.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
-	il_sec=Il.objects.all().values_list()
-	ilce_sec = Ilce.objects.all().values_list()
+	il_sec=Il.objects.all().values()
+	ilce_sec = Ilce.objects.all().values()
 	BOOL_CHOICES = ((True, 'Erkek'), (False, 'Kadın'))
+	
 	ad = forms.CharField(max_length=30, required=False, help_text='Optional.')
 	soyad = forms.CharField(max_length=30, required=False, help_text='Optional.')
 	email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
@@ -15,6 +16,7 @@ class RegistrationForm(UserCreationForm):
 	il = forms.ChoiceField(choices=il_sec, widget=forms.Select(attrs=dict(required=True,placeholder='Il')))
 	ilce = forms.ChoiceField(choices=ilce_sec, widget=forms.Select(attrs=dict(required=True,placeholder='Ilce')))
 	telefon = forms.CharField(max_length =11 ,widget=forms.TextInput(attrs=dict(required=True,placeholder='telefon')))
+	
 	class Meta:
 		model = User
 		fields = ('username','password1', 'password2', 'email','ad', 'soyad', 'cinsiyet', 'dogum_tarihi', 'il','ilce' )
