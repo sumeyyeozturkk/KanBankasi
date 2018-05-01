7bf179739dafd5b3a6606b1854ec0d9abe28cf0f
sumeyyeozturkk <sozturk15@posta.pau.edu.tr>
2018-05-02 00:18:54 +0300

    Kayıtol Formu, kayıt ol metodu eklendi.

--- kanBankasiApp/forms.py
+++ kanBankasiApp/forms.py
@@ -2,3 +2,12 @@ from django import forms
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+class RegistrationForm(UserCreationForm):
+	ad = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	soyad = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+	class Meta:
+		model = User
+		fields = ('username', 'ad', 'soyad', 'email', 'password1', 'password2', )
--- kanBankasiApp/views.py
+++ kanBankasiApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from kanBankasiApp.models import *
+from kanBankasiApp.forms import *
+
 
 
 class HomePageView(generic.ListView):
@@ -7,3 +10,13 @@ class HomePageView(generic.ListView):
 
 	def get_queryset(self):
 		return "helo"
+
+class RegistrationView(generic.FormView):
+	form_class = RegistrationForm
+	template_name = "signup.html"
+	success_url = '/login'
+
+	def form_valid(self, form):
+		form.save()
+		return super().form_valid(form)
+
